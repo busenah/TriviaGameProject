1999337e6eaad762ee7c5df68be8a6e547fede47
Leonard Busenah <busenah@gmail.com>
2019-02-05 15:41:12 -0500

    adding

--- trivia.js
+++ trivia.js
@@ -10,7 +10,9 @@ let states = [
     "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", 
     "Illinois", "Indiana", "Iowa", "Kansas","Kentucky", "Louisiana", "Maine", 
     "Maryland", "Massachusetts", "Michigan","Minnesota", "Mississippi", "Missouri",
-    "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "" ]
+    "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", 
+    "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", 
+    "Pennsylvania", "Rhode Island", ]
 
 
 let correct = document.querySelector(".correct")
