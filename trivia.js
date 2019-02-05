c4535b021af80c71c84c6df95bca9dda91a57789
Leonard Busenah <busenah@gmail.com>
2019-02-05 15:22:24 -0500

    adding

--- trivia.js
+++ trivia.js
@@ -6,10 +6,10 @@ create a container for the capital
 */
 
 let states = [
-    "Alabama", "Alaska", "Arizona", "Arkansas", "California", 
-    "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", 
-    "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas",
-    "Kentucky", "Louisiana", "" ]
+    "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", 
+    "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", 
+    "Illinois", "Indiana", "Iowa", "Kansas","Kentucky", "Louisiana", "Maine", 
+    "Maryland", "Massachusetts", "Michigan","Minnesota", "Mississippi" ]
 
 
 let correct = document.querySelector(".correct")
@@ -18,6 +18,13 @@ let correct = document.querySelector(".correct")
 - If answer is correct, score and console.log("Excellent. You can move to the next question")
 - If not correct, do not score and console.log("Sorry you failed. 
     Please try again or move to the next question")
+    
+*/
+
+
+/*
+At the end of the game, console.log("You have done incredibly well. Please see for youself 
+how much you know about the United States of America")
 
 */
 
