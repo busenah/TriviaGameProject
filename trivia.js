8c6b972e495306a77b2915df22048b6d09fb7883
Leonard Busenah <busenah@gmail.com>
2019-02-05 14:51:49 -0500

    adding

---
+++ trivia.js
@@ -0,0 +1,25 @@
+/*  
+create a container for the Question 
+create a container for the State capital
+
+*/
+
+let score = 0
+let correct = document.querySelector(".correct")
+
+/*
+If answer is correct, score and console.log("Excellent. You can move to the next question")
+If not correct, do not score and console.log("Sorry you failed. 
+You can choose to try again or move to the next question")
+
+
+
+*/
+
+correct.addEventListener('click', () => {
+  score++
+
+  if (score  {
+    scoreEl.innerText = score
+  } else {
+    declareWinner()
