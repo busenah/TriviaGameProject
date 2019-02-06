30bcdcf22542679e8e506d753ccd1d16dab64bd9
Leonard Busenah <busenah@gmail.com>
2019-02-06 15:17:58 -0500

    add

--- trivia.js
+++ trivia.js
@@ -10,69 +10,72 @@ let question = [0];
 
 let answer = ["state"];
 statesAndTheirCapitals[0].state
+statesAndTheirCapitals[0].capital
+
 let statesAndTheirCapitals = [
-    {state: "Alabama", capital: "Montgomery"},
-    {state: "Alaska", capital: "Juneau"},
-    {state: "Arizona", capital: "Phoenix"},
-    {state: "Arkansas", capital: "Little Rock"},
-    {state: "California", capital: "Sacramento"},
-    {state: "Colorado", capital: "Denver"},
-    {state: "Connecticut", capital: "Hartford"},
-    {state: "Delaware", capital: "Dover"},
-    {state: "Florida", capital: "Tallahassee"},
-    {state: "Georgia", capital: "Atlanta"},
-    {state: "Hawaii", capital: "Honolulu"},
-    {state: "Idaho", capital: "Boise"},
-    {state: "Illinois", capital: "Springfield"},
-    {state: "Indiana", capital: "Indianapolis"},
-    {state: "Iowa", capital: "Des Moines"},
-    {state: "Kansas", capital: "Topeka"},
-    {state: "Kentucky", capital: "Frankfort"},
-    {state: "Louisiana", capital: "Baton Rouge"},
-    {state: "Maine", capital: "Augusta"},
-    {state: "Maryland", capital: "Annapolis"},
-    {state: "Massachusetts", capital: "Boston"},
-    {state: "Michigan", capital: "Lansing"},
-    {state: "Minnesota", capital: "St. Paul"},
-    {state: "Mississippi", capital: "Jackson"},
-    {state: "Missouri", capital: "Jefferson City"},
-    {state: "Montana", capital: "Helena"}, 
-    {state: "Nebraska", capital: "Lincoln"}, 
-    {state: "Nevada", capital: "Carson City"}, 
-    {state: "New Hampshire", capital: "Concord"}, 
-    {state: "New Jersey", capital: "Trenton"}, 
-    {state: "New Mexico", capital: "Santa Fe"},
-    {state: "New York", capital: "Albany"}, 
-    {state: "North Carolina", capital: "Raleigh"}, 
-    {state: "North Dakota", capital: "Bismarck"}, 
-    {state: "Ohio", capital: "Columbus"}, 
-    {state: "Oklahoma", capital: "Oklahoma City"}, 
-    {state: "Oregon", capital: "Salem"},
-    {state: "Pennsylvania", capital: "Harrisburg"}, 
-    {state: "Rhode Island", capital: "	Providence"}, 
-    {state: "South Carolina", capital: "Columbia"}, 
-    {state: "South Dakota", capital: "Pierre"},
-    {state: "Tennessee", capital: "Nashville"}, 
-    {state: "Texas", capital: "Austin"}, 
-    {state: "Utah", capital: "Salt Lake City"}, 
-    {state: "Vermont", capital: "Montpelier"}, 
-    {state: "Virginia", capital: "Richmond"}, 
-    {state: "Washington", capital: "Olympia"},
-    {state: "West Virginia", capital: "Charleston"}, 
-    {state: "Wisconsin", capital: "Madison"}, 
-    {state: "Wyoming", capital: "Cheyenne"}, 
-    {state: "District of Columbia", capital: "Washington DC"},
+    { state: "Alabama", capital: "Montgomery" },
+    { state: "Alaska", capital: "Juneau" },
+    { state: "Arizona", capital: "Phoenix" },
+    { state: "Arkansas", capital: "Little Rock" },
+    { state: "California", capital: "Sacramento" },
+    { state: "Colorado", capital: "Denver" },
+    { state: "Connecticut", capital: "Hartford" },
+    { state: "Delaware", capital: "Dover" },
+    { state: "Florida", capital: "Tallahassee" },
+    { state: "Georgia", capital: "Atlanta" },
+    { state: "Hawaii", capital: "Honolulu" },
+    { state: "Idaho", capital: "Boise" },
+    { state: "Illinois", capital: "Springfield" },
+    { state: "Indiana", capital: "Indianapolis" },
+    { state: "Iowa", capital: "Des Moines" },
+    { state: "Kansas", capital: "Topeka" },
+    { state: "Kentucky", capital: "Frankfort" },
+    { state: "Louisiana", capital: "Baton Rouge" },
+    { state: "Maine", capital: "Augusta" },
+    { state: "Maryland", capital: "Annapolis" },
+    { state: "Massachusetts", capital: "Boston" },
+    { state: "Michigan", capital: "Lansing" },
+    { state: "Minnesota", capital: "St. Paul" },
+    { state: "Mississippi", capital: "Jackson" },
+    { state: "Missouri", capital: "Jefferson City" },
+    { state: "Montana", capital: "Helena" },
+    { state: "Nebraska", capital: "Lincoln" },
+    { state: "Nevada", capital: "Carson City" },
+    { state: "New Hampshire", capital: "Concord" },
+    { state: "New Jersey", capital: "Trenton" },
+    { state: "New Mexico", capital: "Santa Fe" },
+    { state: "New York", capital: "Albany" },
+    { state: "North Carolina", capital: "Raleigh" },
+    { state: "North Dakota", capital: "Bismarck" },
+    { state: "Ohio", capital: "Columbus" },
+    { state: "Oklahoma", capital: "Oklahoma City" },
+    { state: "Oregon", capital: "Salem" },
+    { state: "Pennsylvania", capital: "Harrisburg" },
+    { state: "Rhode Island", capital: "	Providence" },
+    { state: "South Carolina", capital: "Columbia" },
+    { state: "South Dakota", capital: "Pierre" },
+    { state: "Tennessee", capital: "Nashville" },
+    { state: "Texas", capital: "Austin" },
+    { state: "Utah", capital: "Salt Lake City" },
+    { state: "Vermont", capital: "Montpelier" },
+    { state: "Virginia", capital: "Richmond" },
+    { state: "Washington", capital: "Olympia" },
+    { state: "West Virginia", capital: "Charleston" },
+    { state: "Wisconsin", capital: "Madison" },
+    { state: "Wyoming", capital: "Cheyenne" },
+    { state: "District of Columbia", capital: "Washington DC" },
 
 ]
 
 let grabQuestion = document.querySelector('#state')
-grabQuestion.innerHTML= "submit"
+grabQuestion.innerHTML = "submit"
 console.log(grabQuestion);
 
 const button = document.querySelector('.js-button')
 
 button.addEventListener('click', function () {
-  alert("You clicked a button!")
+    score = score + 1
+    alert("You clicked a button!")
 })
 
 /*
@@ -101,14 +104,14 @@ how much you know about the United States of America")
 
 */
 
-correct.addEventListener('click', () => {
+button.addEventListener('click', () => {
     score++
 
-    if (score  {
-        score.innerText = score
-    } else {
-        declareScore()
-    
-    }
-    
-
+    /*  if (score  {
+          score.innerText = score
+      } else {
+          declareScore()
+      
+      }
+       */
+});
